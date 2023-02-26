f169af8ac3430cc27c5eb9f92ef8d4debc49d2da
Sakshi Kodgirkar <83027071+KodgirkarSakshi@users.noreply.github.com>
2023-02-26 20:18:45 +0530

    Delete E_Learning directory

--- E_Learning/src/main/java/com/app/Entity/Lecture.java
+++
@@ -1,6 +0,0 @@
-package com.app.Entity;
-
-public class Lecture extends BaseEntity {
-
-	private 
-}
--- E_Learning/src/main/java/com/app/Repository/E_LearningRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//import com.app.Entity.Student;
-
-//public class E_LearningRepo extends JpaRepository<Student, Long> {
-
-//}
