b4288146ecd728afc3f778c2750bb4bd3783ba94
KodgirkarSakshi <kodgirkarsakshi@gmail.com>
2023-02-26 20:26:14 +0530

    Adding renamed project

---
+++ Learn-Ease-with-E-pathshala/src/main/java/com/app/Repository/E_LearningRepo.java
@@ -0,0 +1,9 @@
+package com.app.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import com.app.Entity.Student;
+
+//public class E_LearningRepo extends JpaRepository<Student, Long> {
+
+//}
